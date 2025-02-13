22fefc3c712424c58f73f11312875f67d266adc4
Erick Andrade Ramos <grillo.erick1@gmail.com>
2025-02-12 18:28:38 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
   );
 
   const Wellcome = () => (
-    <section ref={refWellCome} className="pre-page w-full h-full bg-center fixed z-20 bg-(--color-palette-four) bg-[url(./assets/bg.png)]" id="pre_page" onAnimationEnd={Pass}>
+    <section ref={refWellCome} className="pre-page w-full h-full bg-center fixed z-20 bg-(--color-palette-four) bg-[url(./assets/bg.png)] bg-contain" id="pre_page" onAnimationEnd={Pass}>
       <img src={graphLeft} className="absolute left-0 bottom-0 z-10 h-[55%] md:h-[80%] duration-1000 animate-fadeInUp drop-shadow-3xl" id="left_graph" />
       <img src={graphRight} className="absolute right-0 z-10 h-[55%] md:h-[80%] animate-fadeInDown drop-shadow-3xl" id="right_graph" />
 
@@ -78,7 +78,7 @@ function App() {
           <>
             <Wellcome />
             <section className='relative h-lvh w-lvw bg-[url(./assets/wellcome.webp)] bg-no-repeat bg-cover bg-[center_left_20%] xl:bg-[right_top_-350px] 2xl:bg-[right_top_-500px]'>
-              <div className="absolute w-full h-full bg-black/30 backdrop-invert backdrop-opacity-5" />
+              <div className="absolute w-full h-full bg-black/50 backdrop-invert backdrop-opacity-5" />
               <div className='w-full h-full flex justify-center'>
                 <div className='bg-[url(./assets/back-heart.webp)] absolute bottom-20 flex flex-col justify-center items-center w-[250px] h-[250px] bg-no-repeat bg-contain bg-center text-(--color-palette-four) gap-4 drop-shadow-heart md:w-[400px] md:h-[400px] lg:bottom-0'>
                   <p className='font-[GreatVibes] text-4xl md:text-6xl drop-shadow-letter -rotate-12'>Erick &amp; Yola</p>
@@ -86,15 +86,15 @@ function App() {
                 </div>
               </div>
             </section>
-            <section className='relative bg-(--color-palette-one) text-(--color-palette-four) flex gap-3 flex-col p-[3.5rem_2rem] bg-[url(./assets/bg.png)] bg-cover items-center'>
-              <img src={ornament} alt="ornament" className='w-[6rem] h-[6rem] sm:w-[9rem] sm:h-[9rem] absolute top-3 left-3 rotate-[-90deg]' />
-              <img src={ornament} alt="ornament" className='w-[6rem] h-[6rem] sm:w-[9rem] sm:h-[9rem] absolute top-3 right-3' />
-              <img src={ornament} alt="ornament" className='w-[6rem] h-[6rem] sm:w-[9rem] sm:h-[9rem] absolute bottom-3 left-3 rotate-[180deg]' />
-              <img src={ornament} alt="ornament" className='w-[6rem] h-[6rem] sm:w-[9rem] sm:h-[9rem] absolute bottom-3 right-3 rotate-[90deg]' />
-              <p className='font-[GreatVibes] p-[.5rem_0] text-[2.7rem] bg-gradient-to-r from-(--color-palette-three) via-(--color-palette-four) to-(--color-palette-three) text-transparent bg-clip-text sm:text-7xl'>Nuestra Historia</p>
+            <section className='relative text-(--color-palette-four) flex gap-3 flex-col p-[3.5rem_2rem] items-center'>
+              <img src={ornament} alt="ornament" className='w-[7.5rem] h-[7.5rem] sm:w-[9rem] sm:h-[9rem] absolute top-2 left-2 rotate-[-90deg]' />
+              <img src={ornament} alt="ornament" className='w-[7.5rem] h-[7.5rem] sm:w-[9rem] sm:h-[9rem] absolute top-2 right-2' />
+              <img src={ornament} alt="ornament" className='w-[7.5rem] h-[7.5rem] sm:w-[9rem] sm:h-[9rem] absolute bottom-2 left-2 rotate-[180deg]' />
+              <img src={ornament} alt="ornament" className='w-[7.5rem] h-[7.5rem] sm:w-[9rem] sm:h-[9rem] absolute bottom-2 right-2 rotate-[90deg]' />
+              <p className='font-[GreatVibes] p-[.5rem_0] text-[2.7rem] bg-gradient-to-r from-(--color-palette-one) via-(--color-palette-one) to-(--color-palette-one) text-transparent bg-clip-text sm:text-7xl'>Nuestra Historia</p>
               <div className='flex-1 flex flex-col gap-8 items-center sm:mt-5 sm:flex-row max-w-[1280px] sm:justify-around mb-10'>
                 <img src={EY} alt='img' className='w-[230px] h-[230px] sm:w-[300px] sm:h-[300px] lg:w-[400px] lg:h-[400px] rounded-full border-2 border-(--color-palette-three) drop-shadow-3xl' />
-                <div className='font-[Lora] text-center text-(--color-palette-four) max-w-[500px]'>
+                <div className='font-[Lora] text-center text-(--color-palette-one) max-w-[500px]'>
                   <p className='lg:text-lg'>Nuestro amor ha crecido y madurado, a veces de manera sencilla y otras caótica, pero siempre maravillosa.<br /> Comenzaremos una nueva etapa en nuestras vidas, enfrentandonos a nuevos desafíos que nos harán más fuertes y nos ayudarán a crecer juntos el resto de nuestros días.</p>
                 </div>
               </div>
