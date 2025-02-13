734e44f0d63e50501f735f18665d70225583a789
Erick Andrade Ramos <grillo.erick1@gmail.com>
2025-02-12 18:12:00 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import envTopLayer from './assets/env-top-layer.png';
 import graphLeft from './assets/welcome-graph-left.svg';
 import graphRight from './assets/welcome-graph-right.svg';
 import heart from './assets/heart.svg';
+import ornament from './assets/orn.png';
 import EY from './assets/E-Y.webp';
 
 function App() {
@@ -85,12 +86,16 @@ function App() {
                 </div>
               </div>
             </section>
-            <section className='bg-(--color-palette-one) text-(--color-palette-four) flex gap-3 flex-col p-[4rem_1rem] bg-[url(./assets/bg.png)] bg-cover items-center justify-center'>
-              <p className='font-[GreatVibes] p-2 text-center text-5xl bg-gradient-to-r from-(--color-palette-three) via-(--color-palette-four) to-(--color-palette-three) text-transparent bg-clip-text sm:text-7xl'>Nuestra Historia</p>
-              <div className='flex-1 flex flex-col w-full gap-3 justify-center items-center sm:mt-5 sm:flex-row max-w-[1280px] sm:justify-around'>
-                <img src={EY} alt='img' className='w-[250px] h-[250px] sm:w-[300px] sm:h-[300px] lg:w-[400px] lg:h-[400px] rounded-full border-2 border-(--color-palette-three) drop-shadow-3xl' />
-                <div className='relative font-[Lora] text-center w-full bg-(--color-palette-four) rounded-2xl text-(--color-palette-one) border-t-3 border-b-3 border-(--color-palette-three) max-w-[500px]'>
-                  <p className='p-8 antialiased font-stretch-normal text-wrap text-lg'>Nuestro amor ha crecido y madurado, a veces de manera sencilla y otras caótica, pero siempre maravillosa.<br /> Comenzaremos una nueva etapa en nuestras vidas, enfrentandonos a nuevos desafíos que nos harán más fuertes y nos ayudarán a crecer juntos el resto de nuestros días.</p>
+            <section className='relative bg-(--color-palette-one) text-(--color-palette-four) flex gap-3 flex-col p-[3.5rem_2rem] bg-[url(./assets/bg.png)] bg-cover items-center'>
+              <img src={ornament} alt="ornament" className='w-[6rem] h-[6rem] sm:w-[9rem] sm:h-[9rem] absolute top-3 left-3 rotate-[-90deg]' />
+              <img src={ornament} alt="ornament" className='w-[6rem] h-[6rem] sm:w-[9rem] sm:h-[9rem] absolute top-3 right-3' />
+              <img src={ornament} alt="ornament" className='w-[6rem] h-[6rem] sm:w-[9rem] sm:h-[9rem] absolute bottom-3 left-3 rotate-[180deg]' />
+              <img src={ornament} alt="ornament" className='w-[6rem] h-[6rem] sm:w-[9rem] sm:h-[9rem] absolute bottom-3 right-3 rotate-[90deg]' />
+              <p className='font-[GreatVibes] p-[.5rem_0] text-[2.7rem] bg-gradient-to-r from-(--color-palette-three) via-(--color-palette-four) to-(--color-palette-three) text-transparent bg-clip-text sm:text-7xl'>Nuestra Historia</p>
+              <div className='flex-1 flex flex-col gap-8 items-center sm:mt-5 sm:flex-row max-w-[1280px] sm:justify-around mb-10'>
+                <img src={EY} alt='img' className='w-[230px] h-[230px] sm:w-[300px] sm:h-[300px] lg:w-[400px] lg:h-[400px] rounded-full border-2 border-(--color-palette-three) drop-shadow-3xl' />
+                <div className='font-[Lora] text-center text-(--color-palette-four) max-w-[500px]'>
+                  <p className='lg:text-lg'>Nuestro amor ha crecido y madurado, a veces de manera sencilla y otras caótica, pero siempre maravillosa.<br /> Comenzaremos una nueva etapa en nuestras vidas, enfrentandonos a nuevos desafíos que nos harán más fuertes y nos ayudarán a crecer juntos el resto de nuestros días.</p>
                 </div>
               </div>
             </section>
