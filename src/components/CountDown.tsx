6fdf9eecd95eca3d0795f1aab2f41c2f254b2cd8
Erick Andrade Ramos <grillo.erick1@gmail.com>
2025-03-07 11:16:12 -0600

    update

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -30,7 +30,7 @@ export const CountDown = () => {
     const Completionist = () => <span>Te esperamos!</span>;
 
     const Letter = ({ letter, label }: { letter: number, label?: string }) => (
-        <span className='p-5 bg-(--color-palette-one)/50 font-[Lora]  w-[100px] h-[100px] sm:w-[140px] sm:h-[140px] flex flex-col justify-center items-center text-(--color-palette-three) border rounded-lg'>{letter} <span className='text-sm sm:text-xl font-semibold'>{label}</span></span>
+        <span className='p-5 bg-(--color-palette-one)/40 font-[Lora]  w-[75px] h-[75px] sm:w-[140px] sm:h-[140px] flex flex-col justify-center items-center text-(--color-palette-three) border rounded-lg'>{letter} <span className='text-sm sm:text-xl font-semibold'>{label}</span></span>
     );
 
     const renderer = ({ days, hours, minutes, seconds, completed }: CountdownRenderProps) => {
@@ -38,22 +38,22 @@ export const CountDown = () => {
             ? <Completionist />
             :
             <div className='flex flex-wrap text-3xl sm:text-5xl gap-2 items-center justify-center'>
-                <span className='flex items-center gap-2'>
+                {/* <span className='flex items-center gap-2'> */}
                     <Letter letter={days} label='Dias' />
                     <Letter letter={hours} label='Horas' />
-                </span>
-                <span className='flex items-center gap-2'>
+                {/* </span>
+                <span className='flex items-center gap-2'> */}
                     <Letter letter={minutes} label='Minutos' />
                     <Letter letter={seconds} label='Segundos' />
-                </span>
+                {/* </span> */}
             </div>
 
     };
 
     return (
-        <section className='relative h-lvh w-lvw bg-[url(./assets/2.webp)] bg-no-repeat bg-cover bg-[center_left_20%] xl:bg-[right_top_-350px] 2xl:bg-[right_top_-100px]'>
+        <section className='relative h-lvh w-lvw bg-[url(./assets/2.webp)] bg-no-repeat bg-cover bg-center xl:bg-[right_top_-350px] 2xl:bg-[center_top_-500px] pb-[5rem]'>
             <div className="absolute w-full h-full bg-black/60 backdrop-invert backdrop-opacity-10 border-t-4 border-b-4 border-(--color-palette-three)" />
-            <div ref={element} className='w-full h-full flex flex-col justify-center items-center backdrop-opacity-0 sm:gap-6 opacity-0'>
+            <div ref={element} className='w-full h-full flex flex-col justify-end items-center backdrop-opacity-0 sm:gap-6 opacity-0'>
                 <p className='font-[GreatVibes] p-[.5rem_.2rem] text-[3rem] bg-gradient-to-r from-(--color-palette-three) via-(--color-palette-four) to-(--color-palette-three) text-transparent bg-clip-text sm:text-7xl drop-shadow-heart2'>Faltan</p>
                 <Countdown date={new Date("June 14, 2025 13:00:00")} renderer={renderer} />
                 <p className='font-[GreatVibes] p-[.5rem_.2rem] text-[3rem] bg-gradient-to-r from-(--color-palette-three) via-(--color-palette-four) to-(--color-palette-three) text-transparent bg-clip-text sm:text-7xl drop-shadow-heart2'>para nuestra boda</p>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,17 +27,22 @@ export const Footer = () => {
     }, [isVisible, element]);
 
     return (
-        <section className='bg-(--color-palette-one) h-lvh w-lvw p-[1rem] flex flex-col justify-center items-center'>
-            <p className='font-[Lora] p-1 text-xl bg-gradient-to-r from-(--color-palette-four)/80 via-(--color-palette-four)/80 to-(--color-palette-four)/80 text-transparent bg-clip-text sm:text-3xl drop-shadow-heart2 text-center'>Con gran alegría, les invitamos a compartir nuestro día especial mientras unimos nuestras vidas en matrimonio.</p>
-            <p className='font-[GreatVibes] p-[1rem] text-[2.5rem] bg-gradient-to-r from-(--color-palette-three) via-(--color-palette-four) to-(--color-palette-three) text-transparent bg-clip-text sm:text-5xl drop-shadow-heart2'>Erick & Yola</p>
-            <span className='font-[GreatVibes] flex items-center gap-[1rem] text-(--color-palette-four) text-[3.5rem] sm:text-8xl'>
-                <LitteHeart className={{ className: `fill-(--color-palette-three) sm:w-[1.5rem] sm:|h-[1.5rem] animate-bounce-infinite` }} />
-                Los esperamos
-                <LitteHeart className={{ className: `fill-(--color-palette-three) sm:w-[1.5rem] sm:|h-[1.5rem] animate-bounce-infinite` }} />
-            </span>
-            <a className='uppercase border w-[150px] p-4 mt-4 font-[Lora] rounded-full cursor-pointer text-center text-(--color-palette-four)/80' href='#iglesia'>Iglesia</a>
-            <a className='uppercase border w-[150px] p-4 mt-4 font-[Lora] rounded-full cursor-pointer text-center text-(--color-palette-four)/80' href='#salon'>Salón</a>
 
+        <section className='relative h-lvh w-lvw bg-[url(./assets/3.webp)] bg-no-repeat bg-cover bg-[center_left_60%] xl:bg-[right_top_-100px]'>
+            <div className="absolute w-full h-full bg-black/60 backdrop-invert backdrop-opacity-10" />
+            <div ref={element} className='w-full h-full flex flex-col justify-end items-center backdrop-opacity-0 sm:gap-6 opacity-0'>
+                <p className='font-[Lora] p-1 text-lg bg-gradient-to-r from-(--color-palette-four)/80 via-(--color-palette-four)/80 to-(--color-palette-four)/80 text-transparent bg-clip-text sm:text-3xl drop-shadow-heart2 text-center'>Con gran alegría, les invitamos a compartir nuestro día especial mientras unimos nuestras vidas en matrimonio.</p>
+                <p className='font-[GreatVibes] p-[1rem] text-[2.5rem] bg-gradient-to-r from-(--color-palette-three) via-(--color-palette-four) to-(--color-palette-three) text-transparent bg-clip-text sm:text-5xl drop-shadow-heart2'>Erick & Yola</p>
+                <span className='font-[GreatVibes] flex items-center gap-[1rem] text-(--color-palette-four) text-[3.5rem] sm:text-8xl'>
+                    <LitteHeart className={{ className: `fill-(--color-palette-three) sm:w-[1.5rem] sm:|h-[1.5rem] animate-bounce-infinite` }} />
+                    Los esperamos
+                    <LitteHeart className={{ className: `fill-(--color-palette-three) sm:w-[1.5rem] sm:|h-[1.5rem] animate-bounce-infinite` }} />
+                </span>
+                <div className="pt-[1rem] pb-[5rem] flex justify-center items-center gap-4">
+                    <a className='uppercase border w-[150px] p-4 font-[Lora] rounded-full cursor-pointer text-center text-(--color-palette-four)/80' href='#iglesia'>Iglesia</a>
+                    <a className='uppercase border w-[150px] p-4 font-[Lora] rounded-full cursor-pointer text-center text-(--color-palette-four)/80' href='#salon'>Salón</a>
+                </div>
+            </div>
         </section>
     )
 }
